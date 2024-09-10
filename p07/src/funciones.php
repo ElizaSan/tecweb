f0d042d5ffdb7ad4b88c4189432301525b5d6bc3
Miriam <miriam.sanchezga@alumno.buap.mx>
2024-09-10 14:14:35 -0600

    Se crean las funciones numAleatorio, imprimir Variable y esImparOPar

--- p07/src/funciones.php
+++ p07/src/funciones.php
@@ -2,9 +2,31 @@
 $_GET = 'variableGlobal'; // Declare global variable
 $_min = 1;
 $_max = 999;
-function imprimeVariable() {
-    global $_GET; // Access global variable within function scope
-    echo $_GET . " Es tu variable global";
+$_impar;
+$_par;
+
+function randomNum(){
+    $_GET = rand($_min, $_max);
 }
 
+function imprimeVariable() {
+    global $_GET; // Access global variable within function scope
+    echo $_GET . ": es tu variable global";
+}
+
+function esImparOPar($_GET){
+
+    if ($_GET % 2 == 0) {
+        $_GET = $_par;
+        echo "El número $_GET es par.";
+
+    } else {
+        $_GET = $_impar;
+        echo "El número $_GET es impar.";
+    }
+
+}
+
+randomNum();
 imprimeVariable();
+esImparOPar();
