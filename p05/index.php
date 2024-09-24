a2d32161d1eabbbf4a9991ccae62139ed6b895bd
Miriam <miriam.sanchezga@alumno.buap.mx>
2024-09-24 16:05:43 -0600

    Se guardan los documentos plantilla proporcionados por el profesor

--- p05/index.php
+++ p05/index.php
@@ -1,23 +1,28 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-?>
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8">
     <title>Ejercicios PHP</title>
     
 </head>
 <body>
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+?>
+
 
 <h1>Ejercicios PHP</h1>
 
 <?php
 
 
-    echo "<h2> Ejercicio 1</h2>";
-    echo "<b>".'$_myvar: Valida - '."</b>"."Las variables pu    eden empezar con guion bajo.<br>";
-    echo "<br><b>".'$_7var: Valida - '."</b>"."Las variables pueden empezar con guion bajo.<br>";
+    echo "<br><h2> Ejercicio 1</h2>";
+    echo "<b>".'$_myvar: Valida - '."</b>"."Las variables pueden empezar con guion bajo.";
+    echo "<br><b>".'$_7var: Valida - '."</b>"."Las variables pueden empezar con guion bajo.";
     echo "<b>".'myvar: invalida - '."</b>"."Las variables en PHP deben comenzar con un signo de pesos.<br>";
     echo "<b>".'$myvar: Valida - '."</b>"."Las variables en PHP deben comenzar con un signo de pesos.<br>";
     echo "<b>".'$var7: Valida - '."</b>"."Las variable pueden comenzar con signo de pesos y está seguida de letras y un número.<br>";
@@ -132,6 +137,13 @@ header('Content-Type: text/html; charset=UTF-8');
     }
 
     ?>
+
+<p about=""
+     resource="https://www.w3.org/TR/rdfa-syntax"
+     rel="dc:conformsTo" xmlns:dc="http://purl.org/dc/terms/">
+    <a href="https://validator.w3.org/markup/check?uri=referer"><img
+      src="https://www.w3.org/Icons/valid-xhtml-rdfa" alt="Valid XHTML + RDFa" height="31" width="88" /></a>
+  </p>
 
 </body>
 </html>
