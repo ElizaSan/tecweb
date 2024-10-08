c541d99f675e49dc14d37fb780023b4913a39cf7
Miriam <miriam.sanchezga@alumno.buap.mx>
2024-10-07 22:48:26 -0600

    Se corrige el archivo formulario_productos.html

--- p10/validacion.js
+++ p10/validacion.js
@@ -1,65 +1,68 @@
 
 
-function validarNombre() {
+function validarFormulario()() {
+
+    //Variables
     var nombre = document.getElementById("form-name").value;
-    if (nombre.length === 0 || nombre.length > 100) {
-        alert("El nombre no debe ser un campo vacío y debe tener menos de 100 caracteres.");
-        return false;
-    }
-    return true;
-}
-
-function validarMarca() {
-    var nombre = document.getElementById("form-marca").value;
-    if (nombre.length === 0 || nombre.length > 100) {
-        alert("El nombre no debe ser un campo vacío y debe tener menos de 100 caracteres.");
-        return false;
-    }
-    return true;
-}
-
-function validarModelo() {
+    var nombre = document.getElementById("form-name").value;
     var nombre = document.getElementById("form-modelo").value;
-    if (nombre.length === 0 || nombre.length > 100) {
+    var precio = document.getElementById("form-precio").value;
+    var detalle = document.getElementById("form-detalle").value;
+    var unidad = document.getElementById("form-unidad").value;
+    var imagen = document.getElementById("form-imagen").value;
+
+    //NOMBRE
+    if (nombre.trim().length === 0 || nombre.length > 100) {
         alert("El nombre no debe ser un campo vacío y debe tener menos de 100 caracteres.");
         return false;
     }
     return true;
+
+
+    //MARCA
+    if (marca.length === 0 ) {
+        alert("Selecciona una marca.");
+        return false;
+    }
+    return true;
+
+
+    //MODELO
+    if (modelo.length === 0 || modelo.length > 25) {
+        alert("El modelo no debe ser un campo vacío y debe tener menos de 25 caracteres.");
+        return false;
+    }
+    return true;
+
+    //PRECIO
+    if (precio.length === 0 || nombre.length > 99.99) {
+        alert("El precio debe ser mayor a 99.99.");
+        return false;
+    }
+    return true;
+
+    //DETALLE
+    if (detalle.length > 250) {
+        alert("La descripción debe tener menos de 250 caracteres.");
+        return false;
+    }
+    return true;
+
+    //UNIDAD
+    if (unidad.length === 0) {
+        alert("Las unidades deben ser mayores o iguales a 0.");
+        return false;
+    }
+    return true;
+
+    //IMAGEN
+    if (imagen.length === 0) {
+        
+    }
+    return true;
+
+
 }
 
-function validarPrecio() {
-    var nombre = document.getElementById("form-precio").value;
-    if (nombre.length === 0 || nombre.length > 100) {
-        alert("El nombre no debe ser un campo vacío y debe tener menos de 100 caracteres.");
-        return false;
-    }
-    return true;
-}
 
-function validarDetalle() {
-    var nombre = document.getElementById("form-detalle").value;
-    if (nombre.length === 0 || nombre.length > 100) {
-        alert("El nombre no debe ser un campo vacío y debe tener menos de 100 caracteres.");
-        return false;
-    }
-    return true;
-}
-
-function validarUnidad() {
-    var nombre = document.getElementById("form-unidad").value;
-    if (nombre.length === 0 || nombre.length > 100) {
-        alert("El nombre no debe ser un campo vacío y debe tener menos de 100 caracteres.");
-        return false;
-    }
-    return true;
-}
-
-function validarImagen() {
-    var nombre = document.getElementById("form-imagen").value;
-    if (nombre.length === 0 || nombre.length > 100) {
-        alert("El nombre no debe ser un campo vacío y debe tener menos de 100 caracteres.");
-        return false;
-    }
-    return true;
-}
 
